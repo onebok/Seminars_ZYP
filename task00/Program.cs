6b8f2e0252274d81398fa8465a6cea7c0820c9ad
Alexandr <onebok@yandex.ru>
2022-12-20 01:21:01 +0700

    Добавлен знак $

--- task00/Program.cs
+++ task00/Program.cs
@@ -11,4 +11,4 @@
 Console.WriteLine("Введите целое число ");
 int number = Convert.ToInt32(Console.ReadLine());
 int square = number*number;
-Console.WriteLine(square);
+Console.WriteLine($"Квадрат числа {number} = {square}");
