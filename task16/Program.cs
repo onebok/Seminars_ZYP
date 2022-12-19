858b193527dd387914c2f6eb03e2f92d6089683e
Alexandr <onebok@yandex.ru>
2022-12-19 22:18:45 +0700

    Added task00

---
+++ task16/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(IsSquare(number1, number2) ? "да" : "нет");
+
+bool IsSquare(int n1, int n2)
+{
+    return n1 % n2 == 0 || n2 % n1 == 0;
+}
