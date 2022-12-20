30256e4fad9aed3480ba5a4496e41b1866040738
Alexandr <onebok@yandex.ru>
2022-12-20 18:03:06 +0700

    Комит task01

--- task01/Program.cs
+++ task01/Program.cs
@@ -13,3 +13,12 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите второе целое число ");
 int number2 = Convert.ToInt32(Console.ReadLine());
+int square2 = number2*number2;
+if (number1 ==  number2*number2)
+{
+  Console.WriteLine($"Первое число является квадратом второго {number2} = {square2}");
+}
+else
+{
+    Console.WriteLine($"Первое число не является квадратом второго {number2} = {square2}");
+}
