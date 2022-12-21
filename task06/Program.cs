4fdb88e110c19407231756a2df7d0264dfe3292f
Alexandr <onebok@yandex.ru>
2022-12-22 03:34:22 +0700

    Комитим все новые консоли

---
+++ task06/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая 
+// 1. принимает на вход числа
+// и выдает, является ли число четным
+// (делится ли оно на 2 без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number = count
+if (count % 2 = 0)
+{
+    Console.WriteLine("Число четное ");
+}
+// if (number % 2 = 0)
+// {
+//    Console.WriteLine("Число четное ");
+// }
+elsе (count % 2 = 1);
+{
+    Console.WriteLine("Число не четное");
+}
