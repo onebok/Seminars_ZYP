a5ea4c378a9f96b0bd22264d9b675877cebd8334
Alexandr <onebok@yandex.ru>
2022-12-22 02:22:22 +0700

    Комитим комменты task04

--- task04/Program.cs
+++ task04/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. принимает на вход три числа
+// и выдает максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44, 5, 78 -> 78
+// 22, 3, 9 -> 22
