3e7dbc22279100c6af442e09978707ec4e3adfea
Alexandr <onebok@yandex.ru>
2022-12-22 11:38:45 +0700

    Комитим task08

--- task08/Program.cs
+++ task08/Program.cs
@@ -3,3 +3,13 @@
 // а на выходе показывает все четные числа от 1 до N.
 // 5 -> 2, 4
 // 8 -> 2, 4, 6,8
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = number;
+counter = 2;
+while (counter <= number)
+{
+    Console.Write(counter + " ");
+    counter = counter + 2;
+}
