d5eb3be0623bd47488c1eece330487d8b3e1c81b
Alexandr <onebok@yandex.ru>
2022-12-23 00:37:45 +0700

    Комитим task09 стиль кэмэл кейс

--- task09/Program.cs
+++ task09/Program.cs
@@ -10,32 +10,34 @@
 int number = new Random().Next(10, 100);
 Console.WriteLine($"Случайное число из диапазона 10 - 99 -> {number} ");
 
-// int firstdigit =  number / 10;
-// int seconddigit =  number % 10;
-// if (firstdigit > seconddigit) Console.WriteLine($"Наибольшая цифра числа {firstdigit} ");
-// else Console.WriteLine($"Наибольшая цифра числа {seconddigit} ");
+// int firstDigit =  number / 10;
+// int secondDigit =  number % 10;
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {firstDigit} ");
+// else Console.WriteLine($"Наибольшая цифра числа {secondDigit} ");
 
 // Console.Write("Наибольшая цифра числа = ");
 // // интерполяция не нужна $ нет как в 15 строке
 // // Тернарный оператор
-// Console.WriteLine(firstdigit > seconddigit ? firstdigit : seconddigit);
+// Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
 
-// int rezult = firstdigit > seconddigit ? firstdigit : seconddigit;
+// int rezult = firstDigit > secondDigit ? firstDigit : secondDigit;
 // Console.Write("Наибольшая цифра числа = ");
 // Console.WriteLine(rezult);
 
 
 // создаем методы  (функции)
 
-int maxDigit = MaxDigit(number);
+int maxDigit = MaxDigit(number);    // глобальная переменная
 Console.Write($"Наибольшая цифра числа = {maxDigit}");
 
-int MaxDigit(int number)
+int MaxDigit(int num)   // это уже параметр, а не глобальная переменная
 {
-    int firstdigit = number / 10;
-    int seconddigit = number % 10;
-    // int rezult = firstdigit > seconddigit ? firstdigit : seconddigit;
+    int firstDigit = num / 10;
+    int secondDigit = num % 10; // меняем number на num
+    // int rezult = firstDigit > secondDigit ? firstDigit : secondDigit;
     // return rezult;
-    return firstdigit > seconddigit ? firstdigit : seconddigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
 }
 
+// int maxDigit = MaxDigit(number); может располагаться внизу
+// Console.Write($"Наибольшая цифра числа = {maxDigit}");
