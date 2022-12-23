8b83e93cfc20df28dd7bc1a6e6020aea74181ffb
Alexandr <onebok@yandex.ru>
2022-12-23 07:59:52 +0700

    Комитим task16 с тернарным оператором

--- task16/Program.cs
+++ task16/Program.cs
@@ -1,4 +1,14 @@
-﻿Console.WriteLine("введите первое число");
+﻿//  Задача 16.
+// Напишите программу, которая
+// 1. принимает на вход два числа и
+// 2. прооверяет, является ли одно
+// квадратом второго
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8, 9 -> нет
+
+Console.WriteLine("введите первое число");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите второе число");
 int number2 = Convert.ToInt32(Console.ReadLine());
