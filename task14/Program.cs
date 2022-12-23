9ec808a3b1fe6cdb37befbe6af175ba8aeb3491c
Alexandr <onebok@yandex.ru>
2022-12-23 08:59:34 +0700

    Комитим работающую task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -6,5 +6,16 @@
 // 46 -> нет
 // 161 -> да
 
-
-
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number % 7 == 0) & (number % 23 == 0))
+{
+    Console.WriteLine("Число кратно 7 и 23");
+}
+//if((number % 7 == 0) && (number % 23 == 0));
+//Console.WriteLine("Число кратно 7 и 23");
+else
+{
+    Console.WriteLine("Число не кратно 7 и 23");
+}
+// мой компилятор не принимает &&, тернарный оператор не срабатывает
