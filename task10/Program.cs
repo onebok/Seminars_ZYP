6dd7305f04ba2c249d1b77a3cf5ec22f34ec95d6
Alexandr <onebok@yandex.ru>
2022-12-26 00:40:35 +0700

    Комитим task10

--- task10/Program.cs
+++ task10/Program.cs
@@ -7,3 +7,14 @@
 // 918  -> 1
 // 
 
+Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <=1000)
+{
+   Console.WriteLine($"Вторая цифра введенного числа {(number /10)% 10} ");
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
