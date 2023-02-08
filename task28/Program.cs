81953c9a6981b1e5d2eeae905c05742ae7312521
Alexandr <onebok@yandex.ru>
2023-02-08 23:21:39 +0700

    Комитим решение task28 произведение чисел от 1 до N

---
+++ task28/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 28: 
+// Напишите программу, которая 
+// принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = DigitMultiplication(number);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {result} ");
+
+int DigitMultiplication(int num)
+{
+    int multiplic = 1;
+   for (int i = 1; i <= num; i++)
+   {
+        multiplic = multiplic * i;
+   }
+    return multiplic;
+}
+
