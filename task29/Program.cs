e67574626a546230afe86cd57e1cd6c214f873d4
Alexandr <onebok@yandex.ru>
2023-02-09 20:40:28 +0700

    Комитим решение task29 массив из 8 случайных элементов с выводом на экран

---
+++ task29/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 29: 
+// Напишите программу, которая 
+// 1. задаёт массив из 8 элементов и 
+// 2. выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19] 
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+int[] array = new int[8];
+GetArray(array);
+PrintArray(array);
+
+void GetArray(int[] arr)
+{
+ int length = arr.Length;
+ Random rnd = new Random();
+for (int i = 0; i < length; i++)
+{
+    arr [i] = rnd.Next(0,99);
+}
+   
+}
+void PrintArray(int[] array)
+{
+    int count  = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+       Console.Write($"{array[i]} ");
+    }
+       Console.WriteLine();
+}
