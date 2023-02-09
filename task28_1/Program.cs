2a0f4dd63bad816593d5d968bf35976823cc3275
Alexandr <onebok@yandex.ru>
2023-02-09 17:44:11 +0700

    Комитим решение task28_1 фактороиал

---
+++ task28_1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 28: 
+// Напишите программу, которая 
+// принимает на вход число N и 
+// выдаёт произведение чисел от 1 до N.
+// В общем случае, это факториал.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int GetFact(int num)
+{
+    int counter = 1;
+    int res = 1;
+    while (counter <= num)
+    {
+        res = res * counter;
+        counter++;
+    }
+        return res;
+}
+int result = GetFact(Math.Abs(number));
+Console.WriteLine(result);
