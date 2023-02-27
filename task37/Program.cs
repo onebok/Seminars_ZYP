5354da7eba51fa74447008836df1be343650d332
Alexandr <onebok@yandex.ru>
2023-02-27 23:04:21 +0700

    Решение task37 произведение пар чисел в массиве первый последний, второй предпоследний и тд

---
+++ task37/Program.cs
@@ -0,0 +1,53 @@
+﻿
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент,
+// второй и предпоследний и т.д. Результат запишите в новом массиве.
+//[1 2 3 4 5] -> 5 8 3
+//[6 7 3 6] -> 36 21
+
+int[] newArr = RandomArray(7);
+PrintArray(newArr);
+PrintArray(FindSumOfPairs(newArr));
+
+
+int[] FindSumOfPairs(int[] array)
+{
+    int newSize = array.Length / 2;
+
+    if(array.Length % 2 == 1)
+    {
+        newSize++;
+    }
+    int[] result = new int[newSize];
+
+    for(int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - 1 - i];
+    }
+
+    if(array.Length % 2 == 1)
+    {
+        result[newSize -1] = array[array.Length / 2];
+    }
+    return result;
+}
+
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(1,10);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{    
+    foreach(int item in arr)
+    {
+        Console.Write($"{item}, ");
+    } 
+    Console.WriteLine();
+}
