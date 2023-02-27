cbbc0e954f4fff11ccd6621805c51be5a59db8e0
Alexandr <onebok@yandex.ru>
2023-02-27 22:35:34 +0700

    Решение task46 создание матрицы 3х4

---
+++ task46/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 46
+// Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами.  m = 3, n = 4.
+
+Print2DArray(Generate2DArray(3, 4));
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    {
+        for(int i = 0; i < result.GetLength(0); i++)
+        {
+            for(int j = 0; j < result.GetLength(1); j++)
+            {
+                    result[i, j] = new Random().Next(1, 100);
+            }
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+        {
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
