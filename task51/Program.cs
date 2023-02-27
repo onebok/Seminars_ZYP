2e8b66d9afc14533a477092a5338e04e6549b54b
Alexandr <onebok@yandex.ru>
2023-02-27 23:37:26 +0700

    Решение task51 сумма элементов главной диагонали матрицы

---
+++ task51/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+var arr = Generate2DArray(3, 4);
+Print2DArray(arr);
+Console.WriteLine();
+Console.WriteLine(GetSum(arr));
+
+int GetSum(int[,] arr)
+{
+    int length = arr.GetLength(0) < arr.GetLength(1) ? arr.GetLength(0) : 
+
+arr.GetLength(1);
+  
+    int sum = 0;    
+    for(int i = 0; i < arr.GetLength(0); i++)
+    {
+       sum += arr[i, i];    
+    } 
+    return sum;
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    {
+        for(int i = 0; i < result.GetLength(0); i++)
+        {
+            for(int j = 0; j < result.GetLength(1); j++)
+            {
+                    result[i, j] = new Random().Next(1, 100);
+            }
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+        {
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
