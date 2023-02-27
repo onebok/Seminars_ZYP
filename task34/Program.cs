c55fa6b9ce9e5ddbbc9d22e04d54eead38118b8c
Alexandr <onebok@yandex.ru>
2023-02-27 20:21:03 +0700

    Решение task34 количество четных в массиве трехзначных чисел

---
+++ task34/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 34.
+// Задайте массив заполненный случайными 
+// положительными трёхзначными числами. 
+// Напишите программу, 
+// которая покажет количество чётных чисел в массиве.
+
+int[] newArr = RandomArray(10);
+PrintArray(newArr);
+Console.WriteLine(GetMeEven(newArr));
+
+int GetMeEven(int[] arr)
+{
+    int count = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(100,1000);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        Console.Write($"{item}, ");
+    } 
+    Console.WriteLine();
+}
