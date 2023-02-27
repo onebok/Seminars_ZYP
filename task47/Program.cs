4755156193b622b14a5f8a11710b7c074935104a
Alexandr <onebok@yandex.ru>
2023-02-27 22:40:05 +0700

    Решение task47 заполнение матрицы 3х4 вещественными числами

---
+++ task47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+
+Print2DArray(Generate2DArray(3, 4));
+
+double[,] Generate2DArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    {
+        for(int i = 0; i < result.GetLength(0); i++)
+        {
+            for(int j = 0; j < result.GetLength(1); j++)
+            {
+                    result[i, j] = new Random().Next(-10, 10);
+            }
+        }
+    }
+    return result;
+}
+
+void Print2DArray(double[,] arr)
+{
+        {
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
