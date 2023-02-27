44f81edbab5fda6ed822f1eeb5fc408e5e9bc0e7
Alexandr <onebok@yandex.ru>
2023-02-27 22:53:47 +0700

    Решение task49 поиск четных индексов элементов матрицы и возведение их в квадрат в новой матрице

---
+++ task49/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 49: Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+// Например, изначально массив
+// выглядел вот так:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+var arr = Generate2DArray(3, 4);
+Print2DArray(arr);
+Console.WriteLine();
+ModifyArray(arr);
+Print2DArray(arr);
+
+void ModifyArray(int[,] arr)
+{
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+               if(i % 2 == 0 && j % 2 == 0)
+               arr[i, j] = (int)Math.Pow(arr[i, j], 2); 
+            }
+        }
+}
+
+int[,] Generate2DArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    {
+        for(int i = 0; i < result.GetLength(0); i++)
+        {
+            for(int j = 0; j < result.GetLength(1); j++)
+            {
+                    result[i, j] = new Random().Next(1, 100);
+            }
+        }
+    }
+    return result;
+}
+
+void Print2DArray(int[,] arr)
+{
+        {
+        for(int i = 0; i < arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write($"{arr[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
