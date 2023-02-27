c6cb9ef3601ee6bcd6b9ef3bfb1d86c36438a818
Alexandr <onebok@yandex.ru>
2023-02-27 21:22:45 +0700

    Решение task39 реверс массива

---
+++ task39/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// Задача 39.
+// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - 
+// на последнем и т.д.)
+
+
+int[] newArr = RandomArray(5);
+PrintArray(newArr);
+PrintArray(RewersArray(newArr));
+
+int[] RewersArray(int[] arr)
+{
+    int length  = arr.Length;
+    int[] result = new int[length];
+
+    for(int i = 0; i < length; i++)
+    {
+       result[i] = arr[length -1 -i];
+    }
+    return result;
+
+}
+
+
+int[] RandomArray(int length)
+{
+     int[]arr = new int[length];
+     for(int i = 0; i < length; i++)
+     {
+         arr[i] = new Random().Next(1,100);
+     }
+     return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    
+    foreach(int item in arr)
+    {
+        System.Console.Write($"{item}, ");
+    } 
+    System.Console.WriteLine();
+}
