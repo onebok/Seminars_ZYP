b254bccfc5c6c8f147c6f49c3d7e40ce517dbc48
Alexandr <onebok@yandex.ru>
2023-03-02 00:17:43 +0700

    Решение task66 сумма элементов в промежутке между двумя числами

---
+++ task66/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+
+System.Console.WriteLine(SumInterval(1, 15));
+
+int SumInterval(int start, int end)
+{
+    if(start == end)
+    {
+        return start;
+    }
+    return start + (SumInterval(start + 1, end));
+}
