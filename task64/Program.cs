48931a4acea4adbed24132156267437a1085be87
Alexandr <onebok@yandex.ru>
2023-03-02 00:09:13 +0700

    Решение task64 вывод чисел в промежутке

---
+++ task64/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1" 
+
+System.Console.WriteLine(SeriesNumbers(4, 15));
+
+string SeriesNumbers(int start, int end)
+{
+    if(start == end)
+    {
+        return start.ToString();
+    }
+    return start + " " + (SeriesNumbers(start + 1, end));
+}
