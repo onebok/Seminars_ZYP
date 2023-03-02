0d6f29db537884bc6d4afa43b6f3b69b21064a0a
Alexandr <onebok@yandex.ru>
2023-03-02 23:32:46 +0700

    Решение task63 вывод натуральных чисел от заданного числа до 1

--- task63/Program.cs
+++ task63/Program.cs
@@ -10,14 +10,14 @@ int number = Convert.ToInt32(Console.ReadLine());
 NaturalNumbers(number);
 
 void  NaturalNumbers (int num)
+// {
+// 	if (num == 0) return;
+// 	NaturalNumbers(num - 1);
+// 	Console.Write($"{num} "); // вывод по порядку
+// }
+
 {
 	if (num == 0) return;
+	Console.Write($"{num} "); // вывод в обратном порядке
 	NaturalNumbers(num - 1);
-	Console.Write($"{num} "); // вывод по порядку
 }
-
-//{
-//	if (num ==0) return;
-//	Console.Write($"(num) "); // вывод в обратном порядке
-//	NaturalNumbers(num - 1);
-//}
