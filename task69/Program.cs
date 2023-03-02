14d5c5dbed555a0a3d4c21bc99aef3d0e6108d0c
Alexandr <onebok@yandex.ru>
2023-03-03 00:38:54 +0700

    Решение task69 возведение числа в степень при помощи рекурсии

---
+++ task69/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 69.
+// напишите программу, которая на вход принимает
+// два числа A и B, и возводит A в целую 
+// степень B с помощью рекурсии
+// A = 3; B = 5 -> 243 (3^5)
+// A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите основание: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень числа: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = GetPowNum(number1, number2);
+Console.WriteLine(result);
+
+int GetPowNum(int num, int pow)
+{
+    if(pow == 0) return 1;
+    int res = num;
+    res *= GetPowNum(num, pow - 1);
+    return res;
+}
