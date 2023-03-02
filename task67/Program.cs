98afeb191c2f110cbc14438ef570b9a9cf86866d
Alexandr <onebok@yandex.ru>
2023-03-03 00:10:33 +0700

    Решение task67 вывод суммы цифр числа с применением рекурсии

---
+++ task67/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 67.
+// Напишите программу, которая будет принимать
+// на вход число и возвращать сумму его цифр
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int summ = SumOfDigits(number);
+Console.WriteLine(summ);
+
+int SumOfDigits(int num)
+{
+	int sum = num % 10;
+	if(num > 0) sum += SumOfDigits(num / 10);
+	return sum;
+}
