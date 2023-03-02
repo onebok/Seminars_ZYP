ffdd78e9ac47754a25e0df334e8b242bf561f2ed
Alexandr <onebok@yandex.ru>
2023-03-02 23:16:30 +0700

    Решение task65 вывод чисел в промежутке от одного числа до другого

---
+++ task65/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 65.
+// Задайте значения M и N.
+// Напишите программу, которая
+// выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+Console.Write("Введите первое число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите втрое число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbersFromMToN(numberM, numberN);
+
+void  NaturalNumbersFromMToN(int numM, int numN)
+{
+	if (numM < numN)
+	{
+		Console.Write($"{numM} ");
+		NaturalNumbersFromMToN(numM + 1, numN);
+	}
+	if (numM > numN)
+	{
+		Console.Write($"numM ");
+		NaturalNumbersFromMToN(numM - 1, numN);
+	}
+	if  (numN == numM) Console.Write($"{numM} ");
+	}
