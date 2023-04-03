a045161c7f6e3c0fcccab337c8bf56826f21d851
Alexandr <onebok@yandex.ru>
2023-04-03 22:39:11 +0700

    Комитим задачу task64 рекурсия вывода чисел от болбшего до 1

--- task64/Program.cs
+++ task64/Program.cs
@@ -1,14 +1,20 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
-// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
-// N = 5 -> "5, 4, 3, 2, 1" 
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-System.Console.WriteLine(SeriesNumbers(4, 15));
+Console.Write("Введите натуральное число: ");
+double number = Convert.ToDouble(Console.ReadLine());
 
-string SeriesNumbers(int start, int end)
+if (number <= 0 || number % 1 != 0)
+    Console.WriteLine("Число должно быть целым и больше 0");
+else
+    NaturNumbers(number);
+
+void NaturNumbers(double num)
 {
-    if(start == end)
-    {
-        return start.ToString();
-    }
-    return start + " " + (SeriesNumbers(start + 1, end));
+    if (num == 0) return;
+    Console.Write($"{num} ");
+    NaturNumbers(num - 1);
 }
